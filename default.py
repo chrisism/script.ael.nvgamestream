97cde68dfaa01889cce81f16a23049cfa9a17784
chrisism <crizizz@gmail.com>
2024-04-01 16:51:43 +0200

    Fixed int arg

--- default.py
+++ default.py
@@ -23,7 +23,7 @@ from akl.launchers import ExecutionSettings, get_executor_factory
 from resources.lib.launcher import NvidiaGameStreamLauncher
 from resources.lib.scanner import NvidiaStreamScanner
 
-kodilogging.config() 
+kodilogging.config()
 logger = logging.getLogger(__name__)
 
 # --- Addon object (used to access settings) ---
@@ -50,14 +50,14 @@ def run_plugin():
 
     parser = argparse.ArgumentParser(prog='script.akl.nvgamestream')
     parser.add_argument('--cmd', help="Command to execute", choices=['launch', 'scan', 'scrape', 'configure'])
-    parser.add_argument('--type',help="Plugin type", choices=['LAUNCHER', 'SCANNER', 'SCRAPER'], default=constants.AddonType.LAUNCHER.name)
+    parser.add_argument('--type', help="Plugin type", choices=['LAUNCHER', 'SCANNER', 'SCRAPER'], default=constants.AddonType.LAUNCHER.name)
     parser.add_argument('--server_host', type=str, help="Host")
     parser.add_argument('--server_port', type=int, help="Port")
     parser.add_argument('--rom_id', type=str, help="ROM ID")
     parser.add_argument('--romcollection_id', type=str, help="ROM Collection ID")
     parser.add_argument('--source_id', type=str, help="Source ID")
     parser.add_argument('--entity_id', type=str, help="Entity ID")
-    parser.add_argument('--entity_type', type=str, help="Entity Type (ROM|ROMCOLLECTION|SOURCE)")
+    parser.add_argument('--entity_type', type=int, help="Entity Type (ROM|ROMCOLLECTION|SOURCE)")
     parser.add_argument('--akl_addon_id', type=str, help="Addon configuration ID")
     parser.add_argument('--settings', type=json.loads, help="Specific run setting")
 
@@ -68,14 +68,19 @@ def run_plugin():
         kodi.dialog_OK(text=parser.usage)
         return
     
-    if   args.type == constants.AddonType.LAUNCHER.name and args.cmd == 'launch': launch_rom(args)
-    elif args.type == constants.AddonType.LAUNCHER.name and args.cmd == 'configure': configure_launcher(args)
-    elif args.type == constants.AddonType.SCANNER.name  and args.cmd == 'scan': scan_for_roms(args)
-    elif args.type == constants.AddonType.SCANNER.name  and args.cmd == 'configure': configure_scanner(args)
+    if args.type == constants.AddonType.LAUNCHER.name and args.cmd == 'launch':
+        launch_rom(args)
+    elif args.type == constants.AddonType.LAUNCHER.name and args.cmd == 'configure':
+        configure_launcher(args)
+    elif args.type == constants.AddonType.SCANNER.name and args.cmd == 'scan':
+        scan_for_roms(args)
+    elif args.type == constants.AddonType.SCANNER.name and args.cmd == 'configure':
+        configure_scanner(args)
     else:
         kodi.dialog_OK(text=parser.format_help())
     
     logger.debug('Advanced Kodi Launcher Plugin: Nvidia Gamestream -> exit')
+
 
 # ---------------------------------------------------------------------------------------------
 # Launcher methods.
@@ -86,18 +91,18 @@ def launch_rom(args):
     
     try:
         execution_settings = ExecutionSettings()
-        execution_settings.delay_tempo              = settings.getSettingAsInt('delay_tempo')
-        execution_settings.display_launcher_notify  = settings.getSettingAsBool('display_launcher_notify')
-        execution_settings.is_non_blocking          = settings.getSettingAsBool('is_non_blocking')
-        execution_settings.media_state_action       = settings.getSettingAsInt('media_state_action')
-        execution_settings.suspend_audio_engine     = settings.getSettingAsBool('suspend_audio_engine')
-        execution_settings.suspend_screensaver      = settings.getSettingAsBool('suspend_screensaver')
-        execution_settings.suspend_joystick_engine  = settings.getSettingAsBool('suspend_joystick')
+        execution_settings.delay_tempo = settings.getSettingAsInt('delay_tempo')
+        execution_settings.display_launcher_notify = settings.getSettingAsBool('display_launcher_notify')
+        execution_settings.is_non_blocking = settings.getSettingAsBool('is_non_blocking')
+        execution_settings.media_state_action = settings.getSettingAsInt('media_state_action')
+        execution_settings.suspend_audio_engine = settings.getSettingAsBool('suspend_audio_engine')
+        execution_settings.suspend_screensaver = settings.getSettingAsBool('suspend_screensaver')
+        execution_settings.suspend_joystick_engine = settings.getSettingAsBool('suspend_joystick')
                 
         addon_dir = kodi.getAddonDir()
         report_path = addon_dir.pjoin('reports')
         if not report_path.exists():
-            report_path.makedirs()    
+            report_path.makedirs()
         report_path = report_path.pjoin('{}-{}.txt'.format(args.akl_addon_id, args.rom_id))
         
         executor_factory = get_executor_factory(report_path)
@@ -112,17 +117,18 @@ def launch_rom(args):
         launcher.launch()
     except Exception as e:
         logger.error('Exception while executing ROM', exc_info=e)
-        kodi.notify_error('Failed to execute ROM')    
+        kodi.notify_error('Failed to execute ROM')
+
 
 # Arguments: --akl_addon_id --romcollection_id | --rom_id
 def configure_launcher(args):
     logger.debug('Nvidia Gamestream Launcher: Configuring ...')
         
     launcher = NvidiaGameStreamLauncher(
-            args.akl_addon_id,
-            args.rom_id,
-            args.server_host,
-            args.server_port)
+        args.akl_addon_id,
+        args.rom_id,
+        args.server_host,
+        args.server_port)
     
     if launcher.build():
         launcher.store_settings()
@@ -167,9 +173,10 @@ def scan_for_roms(args):
         
     kodi.notify('ROMs scanning done')
 
+
 # Arguments: --akl_addon_id (opt) --romcollection_id
 def configure_scanner(args):
-    logger.debug('Nvidia Gamestream scanner: Configuring ...')    
+    logger.debug('Nvidia Gamestream scanner: Configuring ...')
     addon_dir = kodi.getAddonDir()
     report_path = addon_dir.pjoin('reports')
     
@@ -186,6 +193,7 @@ def configure_scanner(args):
     
     kodi.notify_warn('Cancelled configuring scanner')
 
+
 # ---------------------------------------------------------------------------------------------
 # RUN
 # ---------------------------------------------------------------------------------------------
@@ -194,4 +202,3 @@ try:
 except Exception as ex:
     logger.fatal('Exception in plugin', exc_info=ex)
     kodi.notify_error("General failure")
-    
